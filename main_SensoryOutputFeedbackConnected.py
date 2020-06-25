11f4b4a1d50525a3ca4b29896956b3f65bf3aa50
Heeseung-Lee <heeseung1990@gmail.com>
2020-06-25 16:58:37 +0900

    during_est

--- main_SensoryOutputFeedbackConnected.py
+++ main_SensoryOutputFeedbackConnected.py
@@ -9,7 +9,7 @@ from shutil import copyfile
 import operator
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-nModel = np.array([0, 5])
+nModel = np.array([0, 3])
 iteration = 10000
 stimulus = Stimulus()
 
@@ -26,6 +26,10 @@ connect_p_distant_back = 0.0
 alpha_input = 0.7 	# Chaudhuri et al., Neuron, 2015
 alpha_hidden = 0.2
 alpha_output = 0.5  # Chaudhuri et al., Neuron, 2015; Motor (F1) cortex's decay is in between input and hidden
+delta_delay_update = 0.5 # if estimation errors of consecutive "N_conseq_epoch_est_error" is lower than "criterion_est_error", delay increases by "delta_delay_update"
+criterion_est_error = 8
+N_conseq_epoch_est_error = 10
+
 
 par['n_hidden'] = n_hidden
 par['n_tuned_input'] = n_orituned_neurons
@@ -42,19 +46,21 @@ par['noise_sd'] = noise_sd
 par['alpha_input'] = alpha_input 	# Chaudhuri et al., Neuron, 2015
 par['alpha_hidden'] = alpha_hidden
 par['alpha_output'] = alpha_output  # Chaudhuri et al., Neuron, 2015; Motor (F1) cortex has similar decay profile with sensory cortex
+par['delta_delay_update'] = delta_delay_update
+par['criterion_est_error'] = criterion_est_error
+par['N_conseq_epoch_est_error'] = N_conseq_epoch_est_error
 
 par = update_parameters(par)
 stimulus = Stimulus(par)
 
+# delay = 2.0
 # par['design'].update({'iti'     : (0, 1.5),
 #                       'stim'    : (1.5, 3.0),
-#                       'delay'   : (3.0, 6.0),
-#                       'estim'   : (6.0, 7.5)})
-#
+#                       'delay'   : (3.0, 6.0 + delay),
+#                       'estim'   : (6.0 + delay, 7.5 + delay)})
+# par = update_parameters(par)
+# stimulus = Stimulus(par)
 # trial_info = stimulus.generate_trial()
-#
-# ##
-#
 # fig, axes = plt.subplots(3,1, figsize=(10,8))
 # TEST_TRIAL = np.random.randint(stimulus.batch_size)
 # a0 = axes[0].imshow(trial_info['neural_input'][:,TEST_TRIAL,:].T, aspect='auto'); axes[0].set_title("Neural Input"); fig.colorbar(a0, ax=axes[0])
@@ -82,10 +88,14 @@ def initialize_parameters(iModel, par):
 
     delay = par['design']['delay'][1] - par['design']['delay'][0]
     isavedir = os.path.dirname(os.path.realpath(__file__)) + \
-               '/savedir/connectp_w' + str(connect_p_within) + '_forward_a' + str(connect_p_adjacent_forward) + 'd' + str(connect_p_distant_forward) + \
-               'back_a' + str(connect_p_adjacent_back) + 'd' + str(connect_p_distant_back) + 'scalegamma' + str(scale_gamma) + \
+               '/savedir' \
+               '/connectp_w' + str(connect_p_within) + '_forward_a' + str(connect_p_adjacent_forward) + 'd' + str(connect_p_distant_forward) + \
+                    'back_a' + str(connect_p_adjacent_back) + 'd' + str(connect_p_distant_back) + 'scalegamma' + str(scale_gamma) + \
                '/alpha_in' + str(par['alpha_input']) + '_h' + str(par['alpha_hidden']) + '_out' + str(par['alpha_output']) + \
-               '/nIter' + str(iteration) + 'BatchSize' + str(BatchSize) + '/Delay' + str(delay) + '/iModel' + str(iModel)
+               '/delay_Delta' + str(par['delta_delay_update']) + '_ErrorCri' + str(par['criterion_est_error']) + '_Ntrial' + str(par['N_conseq_epoch_est_error']) + \
+               '/nIter' + str(iteration) + 'BatchSize' + str(BatchSize) + \
+               '/Delay' + str(delay) + \
+               '/iModel' + str(iModel)
     if not os.path.isdir(isavedir):
         os.makedirs(isavedir)
         os.makedirs(isavedir + '/code/')
@@ -127,18 +137,27 @@ def run_model(in_data, syn_x_init, syn_u_init):
     h = np.ones((par['batch_size'], 1)) @ var_dict['h']
     syn_x = syn_x_init
     syn_u = syn_u_init
-    w_rnn = par['EI_mask'] @ (par['modular_sparse_mask'] * tf.nn.relu(var_dict['w_rnn']))
+    w_rnn = par['EI_mask'] @ (par['modular_sparse_mask_initial'] * tf.nn.relu(var_dict['w_rnn']))
 
-    c = 0
-    for rnn_input in in_data:
-        #
+    for c in range(par['n_timesteps']):
+        rnn_input = in_data[c,:,:]
         h, syn_x, syn_u = rnn_cell(rnn_input, h, syn_x, syn_u, w_rnn)
 
         self_h = self_h.write(c, h)
         self_syn_x = self_syn_x.write(c, syn_x)
         self_syn_u = self_syn_u.write(c, syn_u)
         self_output = self_output.write(c, h[:, -par['n_output']:])
-        c += 1
+
+    # c = 0
+    # for rnn_input in in_data:
+    #     #
+    #     h, syn_x, syn_u = rnn_cell(rnn_input, h, syn_x, syn_u, w_rnn)
+    #
+    #     self_h = self_h.write(c, h)
+    #     self_syn_x = self_syn_x.write(c, syn_x)
+    #     self_syn_u = self_syn_u.write(c, syn_u)
+    #     self_output = self_output.write(c, h[:, -par['n_output']:])
+    #     c += 1
     #
     self_h = self_h.stack()
     self_syn_x = self_syn_x.stack()
@@ -166,20 +185,24 @@ def calc_loss(syn_x_init, syn_u_init, in_data, out_target, mask_train):
     loss = par['orientation_cost'] * loss_orient + par['spike_cost'] * spike_loss + par['weight_cost'] * weight_loss
 
     MAP_dirs = output[par['output_rg'][par['output_rg'] > np.max(par['dead_rg'])].min():, :, :]
-    MAP_dirs = tf.nn.log_softmax(MAP_dirs, axis=2).numpy()
-    MAP_dirs = np.sum(MAP_dirs, axis=0)
-    MAP_dirs = np.argmax(MAP_dirs, axis=1) - 1
-    MAP_dirs = par['stim_dirs'][MAP_dirs]
-    Target_dirs = par['stim_dirs'][trial_info['stimulus_ori']]
-    est_error = np.arccos(np.cos(2 * (MAP_dirs - Target_dirs) / 180 * np.pi)) / np.pi * 180 / 2
+    MAP_dirs = tf.nn.log_softmax(MAP_dirs, axis=2)
+    MAP_dirs = tf.reduce_sum(MAP_dirs, axis=0)
+    MAP_dirs = tf.argmax(MAP_dirs, axis=1) - 1
+    # MAP_dirs = MAP_dirs.numpy()
+    stim_dirs = tf.constant(par['stim_dirs'])
+    MAP_dirs = tf.gather(stim_dirs, MAP_dirs)
+    Target_dirs = tf.gather(stim_dirs, trial_info['stimulus_ori'])
+    est_error = tf.math.acos(tf.math.cos(2 * (MAP_dirs - Target_dirs) / 180 * np.pi)) / np.pi * 180 / 2
 
     return loss, loss_orient, spike_loss, weight_loss, loss_orient_print, est_error
 
-def append_model_performance(model_performance, loss, loss_orient, spike_loss, itime, var_dict):
+def append_model_performance(model_performance, loss, loss_orient, spike_loss, est_error, delay, itime, var_dict):
     model_performance['time'].append(itime)
     model_performance['loss'].append(loss)
     model_performance['loss_orient'].append(loss_orient)
     model_performance['spike_loss'].append(spike_loss)
+    model_performance['est_error'].append(est_error)
+    model_performance['delay'].append()
     model_performance['iteration'].append(iteration)
 
     model_performance['w_rnn'].append(var_dict['w_rnn'])
@@ -201,25 +224,26 @@ for iModel in range(nModel[0], nModel[1]):
 
     par, var_dict, var_list, syn_x_init, syn_u_init, batch_size, savedir = initialize_parameters(iModel, par)
     opt = tf.optimizers.Adam(learning_rate=par['learning_rate'])
-    model_performance = {'loss': [], 'loss_orient': [], 'spike_loss': [], 'iteration': [], 'w_in': [],
-                         'w_rnn': [], 'b_rnn': [], 'h': [], 'time': []}
+    model_performance = {'loss': [], 'loss_orient': [], 'spike_loss': [], 'est_error': [],
+                        'iteration': [], 'w_in': [], 'w_rnn': [], 'b_rnn': [], 'h': [], 'time': []}
+    par['modular_sparse_mask_initial'] = par['modular_sparse_mask']
 
     @ tf.function
     def train_onestep(syn_x_init, syn_u_init, in_data, out_target, mask_train):
         with tf.GradientTape() as t:
             loss, loss_orient, spike_loss, weight_loss, loss_orient_print, est_error = calc_loss(syn_x_init, syn_u_init, in_data, out_target, mask_train)
-        grads = t.gradient(loss, var_list)
-        grads_and_vars = list(zip(grads, var_list))
+        vars_and_grads = t.gradient(loss, var_dict)
         capped_gvs = []
-        for grad, var in grads_and_vars:
-            print(var.name)
-            if 'w_rnn' in var.name:
-                grad *= par['w_rnn_mask'] * par['w_rnn_sparse_mask']
+        for var, grad in vars_and_grads.items():
+            if 'w_rnn' in var:
+                print('grad_clip')
+                grad *= par['w_rnn_mask'] * par['modular_sparse_mask_initial']
             capped_gvs.append((tf.clip_by_norm(grad, par['clip_max_grad_val']), var))
         opt.apply_gradients(grads_and_vars=capped_gvs)
         return loss, loss_orient, spike_loss, loss_orient_print, est_error
 
     i = 0
+    cUpdate = 1
     for i in range(0, iteration):
 
         trial_info = stimulus.generate_trial()
@@ -230,16 +254,35 @@ for iModel in range(nModel[0], nModel[1]):
         loss, loss_orient, spike_loss, loss_orient_print, est_error = train_onestep(syn_x_init, syn_u_init, in_data, out_target, mask_train)
 
         itime = np.around((time.time() - t0) / 60, decimals=1)
-        print('iModel=', iModel , ', iter=', i+1,
+        idelay = 1.5 + (cUpdate-1)*par['delta_delay_update']
+        print('iModel=', iModel , ', iter=', i+1, ', delay=', idelay,
               ', loss=', loss.numpy(), ', loss_orient=', np.round(loss_orient.numpy()*par['orientation_cost']),
               ', spike_loss=', np.around(spike_loss.numpy()*par['spike_cost'], decimals=1),
               ', mean_loss_orient=', np.round(10000*loss_orient_print.numpy()*par['orientation_cost']),
+              ', est_error=', est_error,
               ', min=', itime)
 
-        model_performance = append_model_performance(model_performance, loss, loss_orient, spike_loss, itime, var_dict)
+        model_performance = append_model_performance(model_performance, loss, loss_orient, spike_loss, est_error, idelay, itime, var_dict)
 
         if np.mod(i+1, 500) == 0 or (np.mod(i+1, 20) == 0)*(i+1 <= 100) == 1\
                 or (np.mod(i+1, 50) == 0)*(i+1 > 100)*(i+1 <= 500) == 1 or i < 10:
             save_results(model_performance, par, i+1)
+
+        if i > 10:
+            if np.mean(model_performance['est_error'][-par['N_conseq_epoch_est_error']:]) > par['criterion_est_error']:
+                par['design'].update({'iti'     : (0, 1.5),
+                                      'stim'    : (1.5, 3.0),
+                                      'delay'   : (3.0, 3.5 + cUpdate*par['delta_delay_update']),
+                                      'estim'   : (3.5 + cUpdate*par['delta_delay_update'], 5.0 + cUpdate*par['delta_delay_update'])})
+                par = update_parameters(par)
+                stimulus = Stimulus(par)
+                print('##')
+                print(str(cUpdate) + 'nd Delay Update from '
+                      + str(1.5 + (cUpdate-1)*par['delta_delay_update']) +
+                      ' to ' + str(1.5 + cUpdate*par['delta_delay_update']))
+                print('##')
+                cUpdate += cUpdate
+
+
 
     save_results(model_performance, par, i+1)
