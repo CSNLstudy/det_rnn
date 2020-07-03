dd237b348a00126c424f22a516aea421bd24d257
hyunwoogu <haxawalk@gmail.com>
2020-07-03 15:21:40 +0900

    handcraft modified

--- det_rnn/base/_stimulus.py
+++ det_rnn/base/_stimulus.py
@@ -21,7 +21,7 @@ class Stimulus(object):
         neural_input    = self._gen_stim(stimulus_ori)
         desired_output  = self._gen_output(stimulus_ori)
         mask            = self._gen_mask()
-        return {'neural_input'  : neural_input,
+        return {'neural_input'  : neural_input.astype(np.float32),
                 'stimulus_ori'  : stimulus_ori,
                 'desired_output': desired_output,
                 'mask'          : mask}
--- examples/handcraft.py
+++ examples/handcraft.py
@@ -1,43 +1,54 @@
-import sys, copy
-import seaborn as sns
-import pandas as pd
+import copy, sys
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow as tf
 
+sys.path.append('../')
 from det_rnn import *
-import det_rnn.analysis as da
-import det_rnn.train as dt
 
 ##############################################################################
-# Evolution of information with binary codes
+# RNN Pygmalion : Fixed-point evolution of information with binary codes
 ##############################################################################
-stimulus = Stimulus(par)
+
+# WARNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+## This RNN is very sensitive to noise as small as floating-point precision
+## Thus, _stimulus.py should be modified
+## From 
+### return {'neural_input'  : neural_input.astype(np.float32),
+###         'stimulus_ori'  : stimulus_ori,
+###         'desired_output': desired_output.astype(np.float32),
+###         'mask'          : mask}
+
+## To
+### return {'neural_input'  : neural_input,
+###         'stimulus_ori'  : stimulus_ori,
+###         'desired_output': desired_output,
+###         'mask'          : mask}
+##############################################################################
 
 ## Parameters ################################################################
-Nhidden  = 100
-Nrule    = 2
-NruleIn  = 2
-NruleOut = 1
-Nstim    = 24
-Nout     = 24
-pEI      = 0.7  # make sure Ninhibit > Nstim
-overlap  = 3
-dt       = 10
-tau      = 50
-privg    = 1e5
-devi     = 1e-6
-noise_rnn_sd = 0.5 
-batch_size   = 128
+Nhidden  = 100  # number of neurons
+Nrule    = 2    # number of "rule neurons"
+NruleIn  = 2    # number of rule inputs
+NruleOut = 1    # number of rule outputs
+Nstim    = 24   # number of stimulus
+Nout     = 24   # number of stimulus output
+pEI      = 0.7  # make sure Ninhibit > Nstim (I think )
+overlap  = 3    # coding length (ex. if overlap=3 then pi: [...,0,1,1,1,0,...])
+dt       = 10   
+tau      = 50    
+privg    = 1e5  # output "suppression" signal corresponding to "rule neurons"
+devi     = 0.1  # evidence accumulation scaler(speed), acronym for derivative_evidence
+noise_rnn_sd = 0.005  # noise : see model part
+batch_size   = 128   
 
+## Dependent parameters ######################################################
+stimulus = Stimulus(par)
 Nexcite  = int(Nhidden*pEI)
 Ninhibit = Nhidden - Nexcite
 tuning_matrix = stimulus.tuning_input.reshape((24,24))
-# tuning_matrix = stimulus.tuning_input.reshape((24,24)).astype(np.float32) ## float32
 alpha    = dt/tau
 
-
-## Make hidden states ########################################################
+## Make hidden states (eigenvectors) #########################################
 H = np.zeros((Nhidden,Nstim))
 rollvec = np.zeros(Nstim)
 rollvec[:overlap] = 1.
@@ -46,55 +57,54 @@ for i in range(Nstim): H[i+Nexcite,i] = 1.
 
 ## Make Wrnn #################################################################
 ### Initialize
-w_rnn0 = np.random.rand(Nhidden,Nhidden) 
-# w_rnn0 = np.zeros((Nhidden,Nhidden)) 
-w_rnn0[:,Nexcite:] *= (-1.)
-for i in range(Nhidden): w_rnn0[i,i] = -1.
+R = np.random.rand(Nhidden,Nhidden)   ## random backgrounds
+R[:,Nexcite:] *= (-1.)
+for i in range(Nhidden): R[i,i] = -1.
 
-### Make (24,24) block
+### Make excitatory block
 stim_block = np.ones((Nstim,Nstim))
 for i in range(Nstim): stim_block[i,i] = -1.
-w_rnn0[Nrule:(Nrule+Nstim),Nrule:(Nrule+Nstim)] = stim_block
+R[Nrule:(Nrule+Nstim),Nrule:(Nrule+Nstim)] = stim_block
 
-### Make (30,24) block
-w_rnn0[Nexcite:,Nrule:(Nrule+Nstim)] = 1./overlap
+### Make sub-excitatory block
+R[Nexcite:,Nrule:(Nrule+Nstim)] = 1./overlap
 
-### Challenging part
+### Make inhibitory block
 rollvec = -np.ones(Nstim)*overlap
 rollvec[:overlap] = -overlap + 2.
-for i in range(Nstim): w_rnn0[(i+Nrule),Nexcite:(Nexcite+Nstim)] =  np.roll(rollvec,i-int(overlap/2))
+for i in range(Nstim): R[(i+Nrule),Nexcite:(Nexcite+Nstim)] =  np.roll(rollvec,i-int(overlap/2))
 
-### 
-w_rnn0[Nexcite:,Nexcite:(Nexcite+Nstim)] = - np.ones((Ninhibit,Nstim))
-w_rnn0[(Nrule+Nstim):Nexcite,Nrule:(Nrule+Nstim)] = 1.
-w_rnn0[(Nrule+Nstim):Nexcite,Nexcite:(Nexcite+Nstim)] = -float(overlap)
+### Make other blocks
+R[Nexcite:,Nexcite:(Nexcite+Nstim)] = - np.ones((Ninhibit,Nstim))
+R[(Nrule+Nstim):Nexcite,Nrule:(Nrule+Nstim)] = 1.
+R[(Nrule+Nstim):Nexcite,Nexcite:(Nexcite+Nstim)] = -float(overlap)
 
-### Make rule strings(TODO: may not work if Nrule > 2)
-w_rnn0[:Nrule,:] = 0
-w_rnn0[:,:Nrule] = 0
-for i in range(Nrule): w_rnn0[i,i] = -1.
-w_rnn0[0,1] = 1.
-w_rnn0[1,0] = 1.
+### Make block corresponding to "rule neurons"
+R[:Nrule,:] = 0
+R[:,:Nrule] = 0
+for i in range(Nrule): R[i,i] = -1.
+R[0,1] = 1.
+R[1,0] = 1.
 
-### Complete!!!
-w_roll = alpha*w_rnn0 + np.eye(Nhidden)
-w_rnn  = w_rnn0 + np.eye(Nhidden)
+### Make W_rnn
+w_rnn  = R + np.eye(Nhidden)
 
-### Sanity Check
-np.linalg.matrix_rank(w_rnn0)
+### Sanity check
+#### 1. matrix rank
+np.linalg.matrix_rank(R)
 np.linalg.matrix_rank(w_rnn)
-np.linalg.matrix_rank(w_roll)
 
+#### 2. make sure H(respond) are eigenvectors
 abs((w_rnn @ H) - H).sum()
 
+#### 3. make sure H(suppress) are eigenvectors
 _H = copy.deepcopy(H)
 _H[:Nrule,:] = 1
 abs((w_rnn @ _H) - _H).sum()
 
 
-
-## Make Win ##################################################################
-# "Rule 1" is trivial, thus ruled out 
+## Make W_inin ##################################################################
+### 1. Make truncated tuning input matrix ("Rule 1" is trivial, thus ruled out ) 
 Resp_input_trunc = np.zeros(NruleIn - 1 + Nstim); Resp_input_trunc[0] = 0.8
 Resp_h = np.zeros((Nhidden,1)); Resp_h[:NruleIn,:] = -1.
 Htotal = np.concatenate((Resp_h,H*devi),axis=1) # _H : retain, H : respond
@@ -103,37 +113,38 @@ tr_mat_tilde = np.concatenate((Resp_input_trunc.reshape((-1,1)),
                                np.concatenate((np.zeros((1,Nstim)), tuning_matrix))),axis=1)
 tr_mat_full  = np.concatenate((0.8*np.ones((1,NruleIn - 1 + Nstim)),tr_mat_tilde),axis=0)
 
+### 2. Do inversion (very high condition number)
 INV0 = np.linalg.inv(tuning_matrix)
 OneZero = np.zeros((Nstim+1,1)); OneZero[0,0] = 1/0.8
 INV  = np.concatenate((OneZero, np.concatenate((np.zeros((1,Nstim)),INV0))),axis=1)
 
+### 3. Make W_in
 w_in_tilde = Htotal @ INV
 w_in = np.concatenate((np.zeros((Nhidden,1)),w_in_tilde), axis=1)
-abs(w_in_tilde@tr_mat_tilde- Htotal).sum()
+abs(w_in_tilde@tr_mat_tilde- Htotal).sum() # check mapping works
 
 ## Make Wout #################################################################
 w_out = np.zeros((Nhidden,Nout+NruleOut))
-# w_out[Nrule:(Nrule+Nstim),NruleOut:(NruleOut+Nstim)] = H[Nrule:(Nstim+Nrule),:Nstim]
 w_out[Nrule:(Nrule+Nstim),NruleOut:(NruleOut+Nstim)] = tuning_matrix
-w_out[0,0] = privg
+w_out[0,0] = privg ## output "suppression" signal corresponding to "rule neurons"
 
-## var_dict ##################################################################
+## Make var_dict #############################################################
 var_dict = {}
 h0 = np.zeros(Nhidden)
-h0[:Nrule] = 1e-5
+h0[:Nrule] = 1.    ## Note that h0 corresponds to initial suppress rule eigenvector
 var_dict['h'] = h0
 var_dict['w_in'] = w_in
 var_dict['w_rnn'] = w_rnn
 var_dict['w_out'] = w_out
 
-## functions #################################################################
+## Define functions ##########################################################
 def relu(x):
     return np.maximum(x,0.)
 
 def rnn_cell(h, rnn_input, var_dict=var_dict):
     h = relu((1.-alpha)*h + \
-             alpha*(rnn_input @ var_dict['w_in'].T + h @ var_dict['w_rnn'].T))
-             # np.random.normal(size=Nhidden, loc=0,scale=2.*alpha*noise_rnn_sd))
+             alpha*(rnn_input @ var_dict['w_in'].T + h @ var_dict['w_rnn'].T) +\
+              np.random.normal(size=Nhidden, loc=0,scale=2.*alpha*noise_rnn_sd))
     return h
 
 def rnn_model(input_data, var_dict=var_dict):
@@ -153,17 +164,34 @@ def rnn_model(input_data, var_dict=var_dict):
 
 
 ## Run RNN ###################################################################
-par['noise_sd'] = 0
+### Note that W_in has extremely high condition number
+### Meaning that "stimulus noise" may show a snowball-like destructive behavior
+### Therefore, it may not be safe to interpret our RNN is "sensitive to input noise"
+### Sensitivity to stimulus noise may be solely due to W_in instability
+### Therefore we should use par['noise_sd'] = 0
+### Meanwhile, a fairer way to examine noise-sensitivity is "internal noise"
+### Which is modulated by "noise_rnn_sd"
+
+### Define "internal noise"
+noise_rnn_sd = 0.005  ## Feel free to modulate it. As it gets larger, more instable RNN becomes. 
+par['noise_sd'] = 0  
+
+### Update parameters
 par['design'].update({'iti'     : (0,  1.5),
                       'stim'    : (1.5,3.0),
-                      'delay'   : (3.0,89.0),
-                      'estim'   : (89.0,93.5)})
+                        'delay'   : (3.0,4.5),
+                        'estim'   : (4.5,9.0)})       # vanilla setting
+                        # 'delay'   : (3.0,183.0),
+                        # 'estim'   : (183.0,187.5)}) # 3min delay(less noise_rnn_sd recommended)
 par = update_parameters(par)
 stimulus = Stimulus(par)
 trial_info = stimulus.generate_trial()
-Hrnn, Yrnn = rnn_model(trial_info['neural_input'])
-Yrnn_normalize = Yrnn/(np.sum(Yrnn,axis=-1,keepdims=True)+np.finfo(float).eps)
 
+### Run model
+Hrnn, Yrnn = rnn_model(trial_info['neural_input']) # run model
+Yrnn_normalize = Yrnn/(np.sum(Yrnn,axis=-1,keepdims=True)+np.finfo(float).eps) # linear normalization
+
+### Plot!
 fig, axes = plt.subplots(3,1, figsize=(10,8))
 TEST_TRIAL = np.random.randint(par['batch_size'])
 axes[0].imshow(trial_info['neural_input'][:,TEST_TRIAL,:].T, aspect='auto'); axes[0].set_title("Neural Input")
@@ -172,62 +200,78 @@ axes[2].imshow(Yrnn_normalize[:,TEST_TRIAL,:].T,  aspect='auto', vmax=0.13)
 fig.tight_layout(pad=2.0)
 plt.show()
 
-        
 ## Sanity check ###############################################################
-plt.imshow(tr_mat_full)
-plt.imshow(w_in@tr_mat_full)
-
-plt.imshow((H.T @ w_out).T/np.sum(H.T @ w_out, axis=1))
-plt.imshow((_H.T @ w_out).T/np.sum(_H.T @ w_out, axis=1))
+### Visualize designed parameters
+plt.imshow(w_in)   # w_rnn, w_out
 plt.colorbar()
 plt.show()
 
+### "Active" output
+plt.imshow((H.T @ w_out).T/np.sum(H.T @ w_out, axis=1))
+plt.colorbar()
+plt.show()
 
-### 1. Does ITI input induce invariance? : Yes
+### "Suppressed" output
+plt.imshow((_H.T @ w_out).T/np.sum(_H.T @ w_out, axis=1), vmax=0.0001)
+plt.colorbar()
+plt.show()
+
+### Visualization of H: the population codes for each stimulus
+for i in range(24):
+    i_stim_input = tr_mat_full[:,i+1]
+    h = ( w_in @ i_stim_input).reshape((1,-1)) @ var_dict['w_rnn'].T
+    plt.plot(h.T)
+
+
+### 1. Does ITI input induce invariance?
+#### Yes, ITI input(=[1,0,...,0]) maps to [0,0,...,0]
+#### Therefore, ITI period just preservers initial h0
 ITI_input = np.zeros(NruleIn + Nstim);  ITI_input[0] = 0.8
-plt.plot(ITI_input)
-plt.plot(ITI_input @ var_dict['w_in'].T)
-plt.plot(rnn_cell(var_dict['h'], ITI_input))
 
-h = rnn_cell(var_dict['h'], ITI_input)
+plt.plot(ITI_input @ var_dict['w_in'].T)     ## note that ITI input |-> 0
+plt.plot(rnn_cell(var_dict['h'], ITI_input)) ## preservation
+
+h = rnn_cell(var_dict['h'], ITI_input) # starts from h0
 for i in range(2000):
     plt.plot(h)
     h = rnn_cell(h, ITI_input) 
 plt.show()
 
 ### 2. Does stimulus period induce gradual evidence accumulation?
-#### No, instability of population code causes exponential growth
+#### Yes, note the growth of population code as a function of iteration
+#### This growth speed is modulated by the parameter devi 
 stim_input = tr_mat_full[:,1]
 h = rnn_cell(h0, stim_input)
-for i in range(10):
-    plt.plot(h)
-    h = rnn_cell(h, stim_input) 
-plt.show()
-ArithmeticErrorh = rnn_cell(h0, stim_input)
 for i in range(150):
-    plt.plot(h @ var_dict['w_out'])
+    plt.plot(h)
     h = rnn_cell(h, stim_input) 
 plt.show()
 
 ### 3. Does the delay period stably maintain information? 
-#### This depends on "robustness(sparseness)" of Wrnn
-h = rnn_cell(h, ITI_input)
+#### Yes, seems delay input(=[1,0,...,0], same as ITI input) maps to [0,...,0]
+#### Thus, there is no additive information during ITI
+h = rnn_cell(h, ITI_input)  # inherits h from 2.
 for i in range(800):
     plt.plot(h)
     h = rnn_cell(h, ITI_input) 
 plt.show()
 
-for i in range(24):
-    i_stim_input = tr_mat_full[:,i+1]
-    h = ( w_in @ i_stim_input).reshape((1,-1)) @ var_dict['w_rnn'].T
-    plt.plot(h.T)
-
 ### 4. Does the response period evoke response?
-resp_input = copy.deepcopy(ITI_input) 
-resp_input[1] = 0.8
+#### Yes, Response signal input(=[1,1,0,...,0]) maps to [-1,-1,0,...,0]
+#### This diminishes 1st and 2nd component of h (rule neurons)
+#### However, there is lower bound of 0 due to ReLU
+#### In this sense, Resp_input just exploits ReLU
+#### Therefore, during response period, first and second component of h remains 0
+#### This "releases" internal response
+#### However, this RNN is not flexible enough to "re-suppress" response when response cue is nonpresent
+Resp_input = np.zeros(NruleIn + Nstim);  Resp_input[:NruleIn] = 0.8
+h = rnn_cell(h, Resp_input)  # inherits h from 3.
 
+plt.plot(Resp_input @ var_dict['w_in'].T)     ## note that Resp_input |-> [-1,-1,0,...,0]
 
-
-
+for i in range(800):
+    plt.plot(h.reshape((-1,)))
+    h = rnn_cell(h, Resp_input) 
+plt.show()
 
 
