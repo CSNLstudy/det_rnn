0eed278fb9afe33d028dddafc7ff455a06b69351
joshryu0425 <joshryu0425@gmail.com>
2020-07-26 22:05:23 +0900

    vnet initial2

--- vnet/hyper.py
+++ vnet/hyper.py
@@ -7,52 +7,51 @@ __all__ = ['hp', 'hp_spec']
 
 # Model hyperparameters(modifiable)
 hp  = {
-	'sensory_gain': True,
-	'sensory_noise_type': 'Poisson', # 'Normal'
+	# losses and training
+	'loss_spike' : 2e-3, #'spike_cost'  : 2e-3, # todo: not used yet
+	'loss_L1' : 0., # weight regularization
+	'loss_L2' : 0., # weight regularization
+	'loss_MI' : 1, # maximize mutual information
+	'loss_p_smooth' : 1e-1, # laplace filter on the posterior
 
-	
-	
-	'masse' : True,
-	'loss_fun'	: 1, # 0:'mse', 1:'centropy'
-	'mse_weight' : 0.01,
 	'learning_rate' : 2e-2,	  # adam optimizer learning rate
-	'dt'            : 10.,
 	'clip_max_grad_val'  : 0.1,
 
-	'alpha_neuron'  : 0.1, # time constant = dt/alpha ~ i.e. 10/0.1 = 100ms
-	'spike_cost'  : 2e-3,
-	'weight_cost' : 0.,
-	'noise_rnn_sd': 0.5,
+	# neuron
+	'sensory_gain': True,
+	'sensory_noise_type': 'Poisson', # 'Normal'
+	'sensory_efficient': True, # make the prior flat in the sensory space
+	'stim_p': par['stim_p'],  # for efficient coding;
 
-	 # inherited from par (stimulus structures)
-	'n_hidden': par['n_hidden'],
-	'n_input': par['n_input'],
-	'n_tuned_input': par['n_tuned_input'],
-	'n_tuned_output': par['n_tuned_output'],
-	'n_sensory': 30, #number of neurons in the sensory layer
-
-	'n_rule_input': par['n_rule_input'],
-	'n_rule_output': par['n_rule_output'],
-
-	'h0': random_normal_abs((1, par['n_hidden'])),
-	'w_in0': random_normal_abs((par['n_input'], par['n_hidden'])),
-	'w_rnn0': random_normal_abs((par['n_hidden'], par['n_hidden'])),
-	'b_rnn0': np.zeros(par['n_hidden'], dtype=np.float32),
-	'w_out0': random_normal_abs((par['n_hidden'],par['n_output'])) * par['w_out_mask'],
-	'b_out0': np.zeros(par['n_output'], dtype=np.float32),
-
+	'dt': 10.,  # ms
+	'neuron_stsp' : False, # parameter
+	'neuron_tau' : 100,  #'alpha_neuron'  : 0.1, # time constant = dt/alpha ~ i.e. 10/0.1 = 100ms
+	# 'noise_rnn_sd': 0.5, # should we learn this?
 	'syn_x_init': np.ones((par['batch_size'], par['n_hidden']), dtype=np.float32), # do we need to initialize this here?
 	'syn_u_init': np.tile(alternating((0.15, 0.45), par['n_hidden']), (par['batch_size'], 1)), # josh: why alternating?
 
 	'alpha_std': alternating((0.05, 0.00667), par['n_hidden']), # efficacy time constant #josh: what is alternating??
 	'alpha_stf': alternating((0.00667, 0.05), par['n_hidden']), # utilization time constant
+
 	'dynamic_synapse': np.ones(par['n_hidden'], dtype=np.float32),
 	'U': alternating((0.15, 0.45), par['n_hidden']),
 
+	# network; inherited from par (stimulus structures)
+	'n_sensory': 30, #number of neurons in the sensory layer
+	'n_hidden': par['n_hidden'],
+	'n_input': par['n_input'],
+	'n_tuned_input': par['n_tuned_input'],
+	'n_tuned_output': par['n_tuned_output'],
+
+	'n_rule_input': par['n_rule_input'],
+	'n_rule_output': par['n_rule_output'],
+
 	'w_in_mask': np.ones((par['n_input'], par['n_hidden']), dtype=np.float32),
-	'w_rnn_mask': np.ones((par['n_hidden'], par['n_hidden']), dtype=np.float32) - np.eye(par['n_hidden'],dtype=np.float32),
-	'w_out_mask': np.concatenate((np.ones((par['n_exc'], par['n_output']),dtype=np.float32),
-									  np.zeros((par['n_hidden']-par['n_exc'], par['n_output']), dtype=np.float32)),axis=0), # Todo(HL): no input from inhibitory neurons
+	'w_rnn_mask': np.ones((par['n_hidden'], par['n_hidden']), dtype=np.float32) - np.eye(par['n_hidden'],
+																						 dtype=np.float32),
+	'w_out_mask': np.concatenate((np.ones((par['n_exc'], par['n_output']), dtype=np.float32),
+								  np.zeros((par['n_hidden'] - par['n_exc'], par['n_output']), dtype=np.float32)),
+								 axis=0),  # Todo(HL): no input from inhibitory neurons
 
 	'exc_inh_prop': par['exc_inh_prop'],
 	'connect_prob': par['connect_prob']
@@ -62,12 +61,3 @@ if par['modular']:
 	hp['EI_mask'] = modular_mask(hp['connect_prob'], hp['n_hidden'], hp['exc_inh_prop'])
 else:
 	hp['EI_mask'] = w_rnn_mask(hp['n_hidden'], hp['exc_inh_prop'])
-
-# Tensorize hp
-for k, v in hp.items():
-    hp[k] = tf.constant(v, name=k)
-
-# hp_spec
-hp_spec = {}
-for k, v in hp.items():
-	hp_spec[k] = tf.TensorSpec(v.numpy().shape, tf.dtypes.as_dtype(v.numpy().dtype), name=k)
--- vnet/model.py
+++ vnet/model.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import tensorflow_probability as tfp
 from .hyper import *
 import numpy as np
 
@@ -23,6 +22,7 @@ class Model(tf.Module):
         return y, loss
 
     def build(self):
+        # build graph
         self.add_placeholders()
         self.m = self.sensory_layer(self.neural_input)
         self.logits = self.output_layer(self.m)
@@ -39,39 +39,65 @@ class Model(tf.Module):
     def add_placeholders(self):
         # input, rules, etc...
         self.neural_input   = tf.placeholder(tf.float32, shape=[None] + [self.hp['n_tuned_input']]) # batch size and input size
-        self.neural_output  = tf.placeholder(tf.float32, shape=[None] + [self.hp['n_tuned_output']])
+        self.neural_output  = tf.placeholder(tf.float32, shape=[None]) # batchsize, indicate which neuron is the output.
 
     def sensory_layer(self, neural_input):
         # population tuning (1 distribution for vonMises likelihood in the sensory space) neural network to define sensory inputs and outputs
-        sens_u = tf.math.asin(tf.math.sin(tf.layers.dense(neural_input, 1, name='sensory_mu'))) # convert range to -pi to pi
-        sens_k = tf.layers.dense(neural_input, 1, activation=tf.nn.relu, name='sensory_k') # positive kappa
-        sens_g = tf.layers.dense(neural_input, 1, activation=tf.nn.relu, name='sensory_gain')  # positive kappa
+        sens_u = tf.math.asin(tf.math.sin(tf.layers.dense(neural_input, 1, name='sensory_mu',
+                                                          kernel_regularizer = tf.keras.regularizers.l1_l2(l1=self.hp['loss_L1'], l2=self.hp['loss_L2']))
+                                          )) # convert range to -pi to pi
+        sens_k = tf.layers.dense(neural_input, 1, activation=tf.nn.relu, name='sensory_k',
+                                 kernel_regularizer = tf.keras.regularizers.l1_l2(l1=self.hp['loss_L1'], l2=self.hp['loss_L2'])
+                                 ) # positive kappa
 
-        # create a von Mises distribution
+        # Von Mises; todo: check gradient?
         sens_p = tf.math.exp(sens_k * tf.math.cos(self.var_dict['sensory_tunings']- sens_u,
                                                   name='sensory_vm_cos'),
                              name='sensory_vm_exp')/\
                      (tf.constant(2.*np.pi)*tf.math.bessel_i0e(sens_k, name='sensory_vm_bessel'))
-        sens_act = sens_p
-        # sens_act = sens_g * sens_p # todo (?): multiply a constant gain to all input units
 
+        if 'sensory_gain' is True:
+            sens_g = tf.layers.dense(neural_input, 1, activation=tf.nn.relu, name='sensory_gain',
+                                     kernel_regularizer=tf.keras.regularizers.l1_l2(l1=self.hp['loss_L1'],
+                                                                                    l2=self.hp['loss_L2'])
+                                     )  # positive gain
+            sens_act = sens_g * sens_p  # todo (?): multiply a constant gain to all input units
+        else:
+            sens_act = sens_p
+
+        # Sensory measurement. add noise todo: how do we properly scale noise? learn weights for noise, proportional to gain?
         if self.hp['sensory_noise_type'] == 'Normal': # return measurement, with normal noise
             sens_m = tf.random.normal(sens_act.shape, sens_act, tf.sqrt(2 * hp['alpha_neuron']) * hp['noise_rnn_sd'], dtype=tf.float32)
         elif self.hp['sensory_noise_type'] == 'Poisson': # return measurement, with poisson noise
-            sens_m = tf.random.poisson(sens_act.shape, sens_act, dtype=tf.float32, name = 'sensory_noise') # todo: how does the graph work here; does the input change?
+            sens_m = tf.random.poisson(sens_act.shape, sens_act, dtype=tf.float32, name = 'sensory_noise')
+            #todo: how does the graph work here; does the input change?
 
-        return sens_m
+        return sens_m, sens_p, sens_act, sens_g
 
     def output_layer(self,sens_m):
-        logits = tf.nn.relu(tf.layers.dense(sens_m, self.hp['n_tuned_output'], name='sensory_k'))
+        logits = tf.nn.relu(tf.layers.dense(sens_m, self.hp['n_tuned_output'],
+                                            name='sensory_k',
+                                            kernel_regularizer = tf.keras.regularizers.l1_l2(l1=self.hp['loss_L1'], l2=self.hp['loss_L2'])))
         return logits
 
-    def loss(self,labels, logits):
-        tf.nn.softmax_cross_entropy_with_logits(labels, logits, axis=-1, name='loss_CE')
+    def loss(self, labels, logits):
+        # main loss
+        loss_ce = tf.nn.softmax_cross_entropy_with_logits(labels, logits, axis=-1, name='loss_CE')
+
+        # info loss
+        loss_MI = 0. # todo: is there a tractable way to implement this?
+
+        # smoothness loss
+        loss_smoothPost = tf.square(tf.norm(self.laplacian.matmul(logits),ord='euclidean'))
+
+        loss = loss_ce + self.hp['loss_MI'] * loss_MI + self.hp['loss_p_smooth'] * loss_smoothPost
+
+        return loss
+
 
     def optimize_op(self):
         optimizer = tf.train.AdamOptimizer(self.lr)
-        qnet_w = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)im
+        qnet_w = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
 
         grads = optimizer.compute_gradients(self.loss, var_list=qnet_w)
         grad_list = []
@@ -85,24 +111,8 @@ class Model(tf.Module):
         self.train_op = apply_grads  # is this supposed to be a list sess.run(self.train_op)
         self.grad_norm = tf.global_norm(grad_list)
 
-
-    def model(self, input_data, hp):
-
-            return y_stack, h_stack, syn_x_stack, syn_u_stack
-
-    def return_losses(self, input_data, target_data, mask, hp):
-
-        return _Y, {'loss': loss, 'perf_loss': perf_loss, 'spike_loss': spike_loss}
-
-
-    def _train_oneiter(self, input_data, target_data, mask, hp):
-        with tf.GradientTape() as t:
-
-        self.optimizer.apply_gradients(grads_and_vars=capped_gvs)
-        return _Y, {'loss': loss, 'perf_loss': perf_loss, 'spike_loss': spike_loss}
-
-
     def _initialize_variable(self, hp):
+        # todo: change formats for all hp?
         _var_dict = {}
         for k, v in hp.items():
             if k[-1] == '0':  # make this more robust. how to add weights in tf?
@@ -110,7 +120,22 @@ class Model(tf.Module):
                 _var_dict[name] = tf.Variable(hp[k], name=name, dtype='float32')
         # todo: also save other variables?
 
-        # bias for sensory tunings
-        _var_dict['sensory_tunings'] = tf.Variable(np.linspace(-np.pi, np.pi,hp['n_tuned_input']), name='sensory_tunings', dtype='float32')
+        # bias for sensory tunings todo: initialize to something else (i.e. Wei and Stocker transformation)
+        if self.hp['sensory_efficient']:
 
+            cdf = #todo:
+            _var_dict['sensory_tunings'] = tf.convert_to_tensor(np.linspace(-np.pi, np.pi,hp['n_tuned_input']),
+                                                                name='sensory_tunings', dtype='float32')
+        else:
+            _var_dict['sensory_tunings'] = tf.Variable(np.linspace(-np.pi, np.pi,hp['n_tuned_input']),
+                                                       name='sensory_tunings', dtype='float32')
         self.var_dict = _var_dict
+
+        # generate laplacian kernel for loss
+        laplac = np.zeros(self.hp['n_tuned_output'])
+        laplac[0] = -0.25
+        laplac[1] = 0.5
+        laplac[2] = -0.25
+        spectrum = tf.signal.fft(tf.cast(laplac, tf.complex64))
+        self.laplacian = tf.linalg.LinearOperatorCirculant(spectrum, input_output_dtype=tf.float32)
+
