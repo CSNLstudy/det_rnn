be3ff05b935f4b66a6723a65bc559c35a5d20408
hyunwoogu <gentlewalk@snu.ac.kr>
2020-11-03 00:01:50 +0900

    modular added

--- decision_wm/decoder.py
+++ decision_wm/decoder.py
@@ -14,14 +14,27 @@ import det_rnn.analysis as da
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 ##
+par['design'].update({'iti' : (0, 1.5),
+                      'stim' : (1.5, 3.0),                      
+# "Short" (N=20)
+#                      'decision': (4.5, 5.0),
+#                      'delay'   : ((3.0, 4.5), (5.0, 6.0)),
+# "Standard" (N=30)
+#                      'decision': (4.0, 5.5),
+#                      'delay'   : ((3.0, 4.0), (5.5, 6.0)),
+# "Long" (N=20)
+                      'decision': (3.5, 6.0),
+                      'delay'   : (3.0, 3.5),
+                      'estim'   : (6.0, 7.5)})
 par = update_parameters(par)
 stimulus = Stimulus(par)
 trial_info = dt.tensorize_trial(stimulus.generate_trial())
 
+server = 'Data_CSNL-1'
 
 ##
 for i in range(20):
-    model = tf.saved_model.load("/Volumes/Data_CSNL/project/RNN_study/20-10-15/HG/output/decision/success"+str(i))
+    model = tf.saved_model.load("/Volumes/"+server+"/project/RNN_study/20-10-15/HG/output/decision_long/models/success"+str(i))
     stim_list   = np.arange(24)
     ref_list    = np.arange(-4,4+1)
     par['reference'] = ref_list;  par = update_parameters(par)
@@ -45,8 +58,31 @@ for i in range(20):
             H_total[:,(10*9*i_s+10*i_r):(10*9*i_s+10*(i_r+1)),:] = H.numpy()[:,:10,:]
         print(st)
 
+    ## Raw hidden states (down-sampled)
+    ds_timestpes = np.arange(par['n_hidden'], step=10)
+    H_ref  = np.tile(np.repeat(ref_list,10),24)
+    H_raw  = np.ones((len(ds_timestpes), 10 * 24, 9, par['n_hidden'])) * np.nan
+    for i_ref, r_ref in enumerate(ref_list):
+        H_raw[:,:,i_ref,:] = H_total[ds_timestpes,:,:][:,H_ref==r_ref,:]
+    with open('/Volumes/'+server+'/project/RNN_study/20-10-15/HG/output/decision_long/analyses/raw_hidden/sequential' + str(i) + '.pkl', 'wb') as f:
+        pickle.dump(H_raw, f)
 
-    DV = dm_output_total[475,:,2]-dm_output_total[475,:,1]
+
+    ## Univariate
+    H_ref  = np.tile(np.repeat(ref_list,10),24)
+    Univar = np.ones((2,len(ref_list),par['n_timesteps'])) * np.nan
+    for i_ref, r_ref in enumerate(ref_list):
+        Univar[0,i_ref,:] = np.mean(H_total[:,H_ref==r_ref,:75], axis=(1,2))
+        Univar[1,i_ref,:] = np.mean(H_total[:,H_ref==r_ref,75:], axis=(1,2))
+    with open('/Volumes/'+server+'/project/RNN_study/20-10-15/HG/output/decision_long/analyses/univariate/sequential' + str(i) + '.pkl', 'wb') as f:
+        pickle.dump(Univar, f)
+
+
+    ##
+    T_decision = 475 # the middle point
+    DV_L = dm_output_total[T_decision,:,2]
+    DV_R = dm_output_total[T_decision,:,1]
+    DV = DV_L - DV_R
     ground_truth, estim_mean, raw_error, beh_perf = da.behavior_summary({'stimulus_ori': stimulus_ori_total}, em_output_total, par=par)
     corrError = (raw_error - np.pi/2.) % (np.pi) - np.pi/2.
 
@@ -57,9 +93,11 @@ for i in range(20):
              'raw_error'   : raw_error,
              'beh_perf'    : beh_perf,
              'corrError'   : corrError,
+             'DV_L'        : DV_L,
+             'DV_R'        : DV_R,
              'choice'      : DV > 0}
     behav = pd.DataFrame(behav)
-    with open('/Volumes/Data_CSNL/project/RNN_study/20-10-15/HG/output/behavior/sequential'+str(i)+'.pkl', 'wb') as f:
+    with open('/Volumes/'+server+'/project/RNN_study/20-10-15/HG/output/decision_long/analyses//behavior/sequential'+str(i)+'.pkl', 'wb') as f:
         pickle.dump(behav, f)
 
 
@@ -77,7 +115,7 @@ for i in range(20):
                                                                (jstim*10):((jstim+1)*10)])
             Hsimil[i_r,i_t,:,:] = corrHreduc
         print(ref)
-    with open('/Volumes/Data_CSNL/project/RNN_study/20-10-15/HG/output/pattern_similarity/sequential'+str(i)+'.pkl', 'wb') as f:
+    with open('/Volumes/'+server+'/project/RNN_study/20-10-15/HG/output/decision_long/analyses/pattern_similarity/sequential'+str(i)+'.pkl', 'wb') as f:
         pickle.dump(Hsimil, f)
 
     ## Decoding
@@ -123,7 +161,6 @@ for i in range(20):
             stim_decode[i_r, i_t, :] = np.arctan2(readout[curr_trials,:] @ post_sinr, readout[curr_trials,:] @ post_cosr) / 2.
         print(i_t)
     
-    
-    with open('/Volumes/Data_CSNL/project/RNN_study/20-10-15/HG/output/gen_decoded/sequential'+str(i)+'.pkl', 'wb') as f:
+    with open('/Volumes/Data_CSNL-1/project/RNN_study/20-10-15/HG/output/decision_long/analyses/gen_decoded/sequential'+str(i)+'.pkl', 'wb') as f:
         pickle.dump(stim_decode, f)
 
